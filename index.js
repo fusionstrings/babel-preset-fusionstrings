fce07b8a4b7941cedf97290bbdadd3566f17ef53
Dilip Kr. Shukla <contact@fusionstrings.com>
2019-07-10 22:30:41 +0200

    update dependencies

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-module.exports = (api, opts) => ({
-	presets: [
-		[
-			require('@babel/preset-env'),
-			{
-				modules: opts.modules || false,
-				useBuiltIns: 'usage',
-				spec: true,
-				debug: true,
-				targets: {
-					node: 'current',
-					browsers: ['last 2 versions', '> 5%']
-				}
-			}
-		],
-		require('@babel/preset-react'),
-		require('@babel/preset-stage-3')
-	],
-	plugins: [
-		require('@babel/plugin-syntax-dynamic-import'),
-		require('@babel/plugin-proposal-class-properties'),
-		require('@babel/plugin-proposal-object-rest-spread')
-	]
-});
+module.exports = (api, options) => {
+  const {
+    debug = false,
+    modules = "auto",
+    shippedProposals = true,
+    spec = true,
+    useBuiltIns = "entry"
+  } = options;
+
+  return {
+    presets: [
+      [
+        require("@babel/preset-env"),
+        {
+          debug,
+          modules,
+          shippedProposals,
+          spec,
+          useBuiltIns,
+          corejs: {
+            version: 3,
+            proposals: true
+          }
+        }
+      ],
+      require("@babel/preset-react"),
+      require("@babel/preset-typescript")
+    ].filter(Boolean),
+    plugins: [
+      require("@babel/plugin-proposal-class-properties"),
+      require("@babel/plugin-proposal-json-strings"),
+      require("@babel/plugin-syntax-dynamic-import"),
+      require("@babel/plugin-syntax-import-meta"),
+      api.env("test") && require("babel-plugin-dynamic-import-node")
+    ].filter(Boolean)
+  };
+};
